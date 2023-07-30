c0aa65ea34be80e6c05a625c27bc71cd52af2c91
bnjpcson <88189079+bnjpcson@users.noreply.github.com>
2023-07-30 22:55:38 +0800

    disable keys

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 // alert("hello");
 var doc = document;
 
-doc.getElementById('image').addEventListener('click', (ev)=>{
+doc.getElementById('image').addEventListener('click', (ev) => {
     alert("Hello World");
 });
+
+doc.addEventListener('contextmenu', event => event.preventDefault());
+
+doc.onkeydown = function (e) {
+    if (
+        (e.keyCode == 123) || // disable F12 key
+        (e.ctrlKey && e.shiftKey && e.keyCode == 73) || // disable I key
+        (e.ctrlKey && e.shiftKey && e.keyCode == 74) || // disable J key
+        (e.ctrlKey && e.keyCode == 85) // disable U key
+    ) {
+        return false;
+    }
+}
