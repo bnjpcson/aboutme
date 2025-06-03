2222584f0774e4d7142225d91d5d7f83565a30fd
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2025-06-03 09:45:15 +0800

    Added: Add new assets and update styles for footer and project sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,295 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import HomePage from "./pages/HomePage";
 
 function App() {
+  const getTheme = () => {
+    return localStorage.getItem("theme") == null
+      ? window.matchMedia("(prefers-color-scheme: dark)").matches
+        ? "dark"
+        : ""
+      : localStorage.getItem("theme");
+  };
+  const [theme, setTheme] = useState(getTheme());
+  const [active, setActive] = useState("home");
+  const homeSection = useRef(null);
+  const aboutSection = useRef(null);
+  const projectSection = useRef(null);
+  const certificateSection = useRef(null);
+
+  const sunIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+    </svg>
+  );
+
+  const moonIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="w-6 h-6"
+    >
+      <path
+        fillRule="evenodd"
+        d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+
+  const navStyle = (nav) => {
+    if (active == nav) {
+      return "border-b-2 border-black dark:border-white cursor-pointer poppins-semibold";
+    }
+    return "cursor-pointer poppins-regular hover:cursor-pointer";
+  };
+
+  const navStyleMobile = (nav) => {
+    if (active == nav) {
+      return "border-b-2 border-black dark:border-white cursor-pointer poppins-semibold";
+    }
+    return "cursor-pointer poppins-regular hover:cursor-pointer";
+  };
+
+  const changeTheme = () => {
+    if (theme == "") {
+      localStorage.setItem("theme", "dark");
+      setTheme("dark");
+    } else {
+      localStorage.setItem("theme", "");
+      setTheme("");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      const home =
+        homeSection.current.getBoundingClientRect().top + window.scrollY - 150;
+
+      const about =
+        aboutSection.current.getBoundingClientRect().top + window.scrollY - 150;
+
+      const projects =
+        projectSection.current.getBoundingClientRect().top +
+        window.scrollY -
+        150;
+
+      const certificates =
+        certificateSection.current.getBoundingClientRect().top +
+        window.scrollY -
+        150;
+
+      if (scrollY >= home && scrollY < about) {
+        setActive("home");
+      } else if (scrollY >= about && scrollY < projects) {
+        setActive("about");
+      } else if (scrollY >= projects && scrollY < certificates) {
+        setActive("projects");
+      } else if (scrollY > certificates) {
+        setActive("certificates");
+      }
+    });
+  }, []);
+
+  const scrollTo = (offsetTop) => {
+    window.scrollTo({
+      top: offsetTop - 100,
+      behavior: "smooth",
+    });
+    document.getElementById("my-drawer-3").checked = false;
+  };
+
+  const LogoBtn = (
+    <a className="btn btn-ghost">
+      <div className="text-[24px] poppins-bold">
+        <span className="tracking-widest">Bnj</span>
+        <span className="text-primary tracking-widest">pcson</span>
+      </div>
+    </a>
+  );
+
   return (
     <>
-      <div className="text-[18px] md:text-[16px]">
-        <HomePage />
+      <div className={theme}>
+        <div
+          className={`bg-light text-dark dark:bg-dark dark:text-light min-h-screen min-w-min`}
+        >
+          <div className="drawer">
+            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+            <div className="drawer-content flex flex-col">
+              {/* Navbar */}
+              <div className="navbar fixed bg-light dark:bg-dark  shadow-md z-10">
+                <div className="flex-none lg:hidden">
+                  <label
+                    htmlFor="my-drawer-3"
+                    aria-label="open sidebar"
+                    className="btn btn-square btn-ghost"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      className="inline-block h-6 w-6 stroke-current"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      ></path>
+                    </svg>
+                  </label>
+                </div>
+                <div className="flex flex-row w-full">
+                  {LogoBtn}
+                  <div className="flex flex-row w-full">
+                    <div className="hidden flex-none lg:block w-full lg:ml-12">
+                      <ul className="menu menu-horizontal  gap-5 flex flex-row">
+                        {/* Navbar menu content here */}
+                        <li className={navStyle("home")}>
+                          <a
+                            href="#home"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              scrollTo(homeSection.current.offsetTop);
+                            }}
+                          >
+                            Home
+                          </a>
+                        </li>
+                        <li className={navStyle("about")}>
+                          <a
+                            href="#about"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              scrollTo(aboutSection.current.offsetTop);
+                            }}
+                          >
+                            About
+                          </a>
+                        </li>
+                        <li className={navStyle("projects")}>
+                          <a
+                            href="#projects"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              scrollTo(projectSection.current.offsetTop);
+                            }}
+                          >
+                            Projects
+                          </a>
+                        </li>
+                        <li className={navStyle("certificates")}>
+                          <a
+                            href="#certificates"
+                            onClick={(e) => {
+                              e.preventDefault();
+                              scrollTo(certificateSection.current.offsetTop);
+                            }}
+                          >
+                            Certificates
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                  <div>
+                    <button
+                      className="btn btn-ghost"
+                      onClick={() => {
+                        changeTheme();
+                      }}
+                    >
+                      {theme == "dark" ? sunIcon : moonIcon}
+                    </button>
+                  </div>
+                </div>
+              </div>
+              {/* Page content here */}
+              <div className="text-[18px] md:text-[16px]">
+                <HomePage
+                  theme={theme}
+                  setTheme={setTheme}
+                  active={active}
+                  setActive={setActive}
+                  scrollTo={scrollTo}
+                  homeSection={homeSection}
+                  aboutSection={aboutSection}
+                  projectSection={projectSection}
+                  certificateSection={certificateSection}
+                  LogoBtn={LogoBtn}
+                />
+              </div>
+            </div>
+            <div className="drawer-side z-20">
+              <label
+                htmlFor="my-drawer-3"
+                aria-label="close sidebar"
+                className="drawer-overlay"
+              ></label>
+              <ul
+                className={`menu bg-light text-dark dark:bg-dark dark:text-light min-h-full w-80 p-0`}
+              >
+                {/* Sidebar content here */}
+                <div className="p-0 h-16 shadow-md flex flex-col justify-center items-start">
+                  {LogoBtn}
+                </div>
+                <div className="p-4 flex flex-col gap-2">
+                  <li className={navStyleMobile("home")}>
+                    <a
+                      href=""
+                      onClick={(e) => {
+                        e.preventDefault();
+                        scrollTo(homeSection.current.offsetTop);
+                      }}
+                    >
+                      Home
+                    </a>
+                  </li>
+                  <li className={navStyleMobile("about")}>
+                    <a
+                      href=""
+                      onClick={(e) => {
+                        e.preventDefault();
+                        scrollTo(aboutSection.current.offsetTop);
+                      }}
+                    >
+                      About
+                    </a>
+                  </li>
+                  <li className={navStyleMobile("projects")}>
+                    <a
+                      href=""
+                      onClick={(e) => {
+                        e.preventDefault();
+                        scrollTo(projectSection.current.offsetTop);
+                      }}
+                    >
+                      Projects
+                    </a>
+                  </li>
+                  <li className={navStyleMobile("certificates")}>
+                    <a
+                      href=""
+                      onClick={(e) => {
+                        e.preventDefault();
+                        scrollTo(certificateSection.current.offsetTop);
+                      }}
+                    >
+                      Certificates
+                    </a>
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/sections/AboutSection.jsx
+++ src/sections/AboutSection.jsx
@@ -1,178 +1,94 @@
-import React from "react";
-
-class Experience {
-  constructor(date, role, company, duties) {
-    this.date = date;
-    this.role = role;
-    this.company = company;
-    this.duties = duties;
-  }
-
-  getObject() {
-    return {
-      date: this.date,
-      role: this.role,
-      company: this.company,
-      duties: this.duties,
-    };
-  }
-}
+import React, { useEffect, useState } from "react";
 
 const AboutSection = ({ theme }) => {
-
-  const fullSuiteExperience = new Experience("", "", "", []);
-  const addessaExperience = new Experience("", "", "", []);
-  const internExperience = new Experience("", "", "", []);
-
-  const experiences = [
-    internExperience.getObject(),
-    addessaExperience.getObject(),
-    fullSuiteExperience.getObject(),
+  const experiencesData = [
+    {
+      date: "March 2024 - Present",
+      role: "Software Engineer",
+      company: "FullSuite",
+      duties: [
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt nihil ratione ex saepe laboriosam tempore itaque, laudantium neque, quidem dignissimos qui minima reiciendis natus obcaecati magni aperiam doloribus aliquid commodi!",
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam quos quam dicta quis quaerat a dolores cupiditate officiis, accusantium sequi? Ratione, quod nihil? Rem, reiciendis at obcaecati eligendi nesciunt qui?",
+      ],
+    },
+    {
+      date: "August 2023 - February 2024",
+      role: "Web Developer",
+      company: "Addessa Corporation",
+      duties: [
+        "Enhanced company's websites by implementing additional functionalities using Laravel and jQuery.",
+        "Revamped company's websites using Laravel for backend development and Vue.js for frontend design.",
+        "Ensure and maintained the continuous functionalities of all assigned company websites.",
+        "Backed up files and conducted regular monitoring of website statistics and status on a web hosting platform.",
+        "Provides technical assistance and support to colleagues for resolving company-related technical issues and problems.",
+      ],
+    },
+    {
+      date: "March 2023 - June 2023",
+      role: "Intern",
+      company: "Global Agility Solutions",
+      duties: [
+        "Designed and developed a company's Web-based ID System using Laravel PHP Framework",
+        "Designed and developed a company's ID System using C# Windows Form",
+        "Provides troubleshooting and maintenance support for PCs and laptops, diagnosing and resolving hardware and software issues to ensure optimal functionality.",
+      ],
+    },
   ];
 
-  console.log(experiences);
+  const [experiences, setExperiences] = useState(experiencesData);
+
+  useEffect(() => {
+    console.log(experiences);
+  }, []);
 
   return (
     <section
       id="about"
       className="flex flex-col items-center w-full bg-black max-w-[1920px]"
     >
-      <div className="flex flex-col md:px-50 lg:px-60 md:px-20 sm:px-10 px-5 items-center text-light min-h-screen  max-w-5xl">
-        <h1 className="poppins-semibold text-4xl md:text-3xl">
+      <div className="flex flex-col px-5 items-start text-light min-h-screen max-w-5xl pb-20">
+        <h1 className="poppins-semibold text-5xl md:text-4xl">
           <span className="text-primary">#</span> About
         </h1>
         <div className="divider"></div>
         <div className="space-y-8 relative before:absolute before:inset-0 before:ml-5 before:-translate-x-px md:before:ml-[8.75rem] md:before:translate-x-0 before:h-full before:w-0.5 before:bg-gradient-to-b before:from-transparent before:via-slate-300 before:to-transparent w-full">
-          <div className="relative">
-            <div className="md:flex items-center md:space-x-4 mb-3">
-              <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
-                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-10 h-10 text-primary"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+          {experiences.map((experience, index) => {
+            return (
+              <div className="relative" key={`experience-${index}`}>
+                <div className="md:flex items-center md:space-x-4 mb-3">
+                  <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
+                    <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        className="w-10 h-10 text-primary"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </div>
+                    <time className="font-caveat font-medium  md:w-28">
+                      {experience.date}
+                    </time>
+                  </div>
+                  <h1 className="poppins-medium text-xl ml-14">
+                    {experience.role}
+                  </h1>
+                  <div className="ml-14 italic">{experience.company}</div>
                 </div>
-                <time className="font-caveat font-medium  md:w-28">
-                  March 2024 - Present
-                </time>
-              </div>
-              <h1 className="poppins-medium text-xl ml-14">
-                Software Engineer
-              </h1>
-              <div className="ml-14 italic">FullSuite</div>
-            </div>
-            <div className=" text-slate-400 ml-14 md:ml-44 text-justify">
-              <li>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Nesciunt nihil ratione ex saepe laboriosam tempore itaque,
-                laudantium neque, quidem dignissimos qui minima reiciendis natus
-                obcaecati magni aperiam doloribus aliquid commodi!
-              </li>
-              <li>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Quibusdam quos quam dicta quis quaerat a dolores cupiditate
-                officiis, accusantium sequi? Ratione, quod nihil? Rem,
-                reiciendis at obcaecati eligendi nesciunt qui?
-              </li>
-            </div>
-          </div>
-          <div className="relative">
-            <div className="md:flex items-center md:space-x-4 mb-3">
-              <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
-                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-10 h-10 text-primary"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                <div className=" text-slate-400 ml-14 md:ml-44 text-justify">
+                  {experience.duties.map((duty, index) => {
+                    return <li key={`duty-${index}`}>{duty}</li>;
+                  })}
                 </div>
-                <time className="font-caveat font-medium  md:w-28">
-                  August 2023 - February 2024
-                </time>
               </div>
-              <h1 className="poppins-bold text-xl ml-14">Web Developer</h1>
-              <div className="ml-14 italic">Addessa Corporation</div>
-            </div>
-            <div className=" text-slate-400 ml-14 md:ml-44">
-              <li>
-                Enhanced company's websites by implementing additional
-                functionalities using Laravel and jQuery.
-              </li>
-              <li>
-                Revamped company's websites using Laravel for backend
-                development and Vue.js for frontend design.
-              </li>
-              <li>
-                Ensure and maintained the continuous functionalities of all
-                assigned company websites.
-              </li>
-              <li>
-                Backed up files and conducted regular monitoring of website
-                statistics and status on a web hosting platform.
-              </li>
-              <li>
-                Provides technical assistance and support to colleagues for
-                resolving company-related technical issues and problems.
-              </li>
-            </div>
-          </div>
-          <div className="relative">
-            <div className="md:flex items-center md:space-x-4 mb-3">
-              <div className="flex items-center space-x-4 md:space-x-2 md:space-x-reverse">
-                <div className="flex items-center justify-center w-10 h-10 rounded-full bg-white shadow md:order-1">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="currentColor"
-                    className="w-10 h-10 text-primary"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12Zm13.36-1.814a.75.75 0 1 0-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 0 0-1.06 1.06l2.25 2.25a.75.75 0 0 0 1.14-.094l3.75-5.25Z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                </div>
-                <time className="font-caveat font-medium  md:w-28">
-                  March 2023 - June 2023
-                </time>
-              </div>
-              <h1 className="poppins-bold text-xl ml-14">Intern</h1>
-              <div className="ml-14 italic">Global Agility Solutions</div>
-            </div>
-            <div className=" text-slate-400 ml-14 md:ml-44">
-              <li>
-                Designed and developed a company's Web-based ID System using
-                Laravel PHP Framework
-              </li>
-              <li>
-                Designed and developed a company's ID System using C# Windows
-                Form
-              </li>
-              <li>
-                Provides troubleshooting and maintenance support for PCs and
-                laptops, diagnosing and resolving hardware and software issues
-                to ensure optimal functionality.
-              </li>
-            </div>
-          </div>
+            );
+          })}
         </div>
-        <div className="divider"></div>
       </div>
     </section>
   );
