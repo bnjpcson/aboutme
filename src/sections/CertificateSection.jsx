8b438c24b7d8064087e0cd47ff8374b818f248b6
BenjiePecson <161810296+BenjiePecson@users.noreply.github.com>
2025-06-06 11:53:47 +0800

    Added: Featured Gallery

--- src/sections/CertificateSection.jsx
+++ src/sections/CertificateSection.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import profile from "../assets/images/image1.jpg";
 import UnderConstructionComponent from "../components/UnderConstructionComponent";
 import ShinyText from "../reactbits/ShinyText";
+import { FeaturedImageGallery } from "../components/FeaturedImageGallery";
 
 const CertificateSection = ({ theme }) => {
   return (
@@ -9,8 +10,8 @@ const CertificateSection = ({ theme }) => {
       id="certificates"
       className="flex flex-col items-center w-full bg-black max-w-[1920px]"
     >
-      <div className="flex flex-col px-5 py-10 text-light min-h-screen max-w-5xl items-center">
-        <h1 className="poppins-semibold text-5xl md:text-4xl text-center flex items-center gap-3">
+      <div className="flex flex-col px-5 py-10 text-light min-h-screen items-center">
+        <h1 className="poppins-semibold text-5xl md:text-4xl text-center flex items-center justify-center gap-3">
           <span className={`text-white`}>#</span>
           <ShinyText
             text={"Certificates"}
@@ -19,7 +20,9 @@ const CertificateSection = ({ theme }) => {
           />
         </h1>
         <div className="divider"></div>
-        <UnderConstructionComponent />
+        <div className="w-full">
+          <FeaturedImageGallery />
+        </div>
         <div className="divider"></div>
       </div>
     </section>
